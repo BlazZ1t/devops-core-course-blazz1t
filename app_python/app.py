362b1830165893f834b6dce8ef5a3be7b5b4bfc5
BlazZ1t <k.chistyakov@innopolis.university>
2026-02-12 15:15:47 +0300

    Add tests and github actions workflow

--- app_python/app.py
+++ app_python/app.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Request
 import os
 import socket
 import platform
-import time, datetime
-import json
+import time
+import datetime
 from contextlib import asynccontextmanager
 
 SERVICE_NAME = os.getenv("SERVICE_NAME", "devops-info-service")
@@ -13,6 +13,7 @@ START_TIME_UTC = round(time.time())
 HOST = os.getenv('HOST', '0.0.0.0')
 PORT = int(os.getenv('PORT', 5000))
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -24,28 +25,31 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     docs_url=None,
-    
     redoc_url=None,
     openapi_url=None,
     lifespan=lifespan
 )
 
+
 def get_system_info():
     return {
-        "hostname" : socket.gethostname(),
-        "platform" : platform.system(),
-        "architecture" : platform.machine(),
-        "python_version" : platform.python_version()
+        "hostname": socket.gethostname(),
+        "platform": platform.system(),
+        "architecture": platform.machine(),
+        "python_version": platform.python_version()
     }
+
 
 def get_runtime_info():
     print("Startup seconds are ", START_TIME_UTC)
     return {
-        "uptime_seconds" : round(time.time()) - START_TIME_UTC,
-        "uptime_human" : str(datetime.timedelta(seconds=round(time.time()) - START_TIME_UTC)),
-        "current_time" : time.strftime("%H hours %M minutes", time.localtime()),
-        "timezone" : time.timezone
+        "uptime_seconds": round(time.time()) - START_TIME_UTC,
+        "uptime_human": str(datetime.timedelta(seconds=round(time.time())
+                                               - START_TIME_UTC)),
+        "current_time": time.strftime("%H hours %M minutes", time.localtime()),
+        "timezone": time.timezone
     }
+
 
 def get_endpoints():
     result = []
@@ -53,40 +57,41 @@ def get_endpoints():
     for route in app.routes:
         result.append(
             {
-                "path" : route.path,
-                "method" : list(route.methods)[0],
-                "description" : "" 
+                "path": route.path,
+                "method": list(route.methods)[0],
+                "description": ""
             }
         )
-    
+
     return result
 
+
 @app.get("/")
-async def get_server_info(request: Request):
+def get_server_info(request: Request):
     get_endpoints()
     return {
         "service": {
-            "name" : SERVICE_NAME,
-            "version" : VERSION,
-            "description" : "DevOps course info service",
-            "framework" : "FastAPI"
+            "name": SERVICE_NAME,
+            "version": VERSION,
+            "description": "DevOps course info service",
+            "framework": "FastAPI"
         },
-        "system" : get_system_info(),
-        "runtime" : get_runtime_info(),
-        "request" : {
-            "client_ip" : request.client.host,
-            "user_agent" : request.headers.get("user-Agent"),
-            "method" : request.method,
-            "path" : request.url.path
+        "system": get_system_info(),
+        "runtime": get_runtime_info(),
+        "request": {
+            "client_ip": request.client.host,
+            "user_agent": request.headers.get("user-Agent"),
+            "method": request.method,
+            "path": request.url.path
         },
-        "endpoints" : get_endpoints()
+        "endpoints": get_endpoints()
     }
 
 
 @app.get("/health")
-async def get_health():
+def get_health():
     return {
-        "status" : "healthy",
-        "timestamp" : datetime.datetime.now().isoformat(),
-        "uptime_seconds" : round(time.time()) - START_TIME_UTC
+        "status": "healthy",
+        "timestamp": datetime.datetime.now().isoformat(),
+        "uptime_seconds": round(time.time()) - START_TIME_UTC
     }
---
+++ app_python/docs/LAB03.md
@@ -0,0 +1,7 @@
+## Testing
+
+- Pytest was chosen for its speed and ease of use
+- Tests are structured as file per function, in the tests/ folder
+- To run tests locally first run `pip install -r app_python/requirements-dev.txt` and then run `pytest .`
+
+![Alt text](screenshots/04-test-output.png)
